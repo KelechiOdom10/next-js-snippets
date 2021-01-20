50727ecd45222cd675c22de77440e29b9cb4065f
kelechiodom10 <kelechiodom10@gmail.com>
2021-01-20 14:23:22 +0000

    Created a wrapper component for the code.js component

---
+++ components/CodeWrapper.js
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+function CodeWrapper(props) {
+	return <Box position="relative" {...props} />;
+}
+
+export default CodeWrapper;
