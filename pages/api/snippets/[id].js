3e100c78cc8baa591fba507f79330d90775188ac
kelechiodom10 <kelechiodom10@gmail.com>
2021-01-20 15:05:15 +0000

    Created snippets api routes

---
+++ pages/api/snippets/[id].js
@@ -0,0 +1,74 @@
+const db = require("../../../lib/db").instance;
+import { handler } from "../../../handlers";
+import nc from "next-connect";
+import { authorize } from "../../../middleware/authorize";
+
+const authorization = nc()
+  .put("/api/snippets/:id", authorize)
+  .delete("/api/snippets/:id", authorize);
+
+export default handler
+  .use(authorization)
+  .get(async (req, res) => {
+    const { id } = req.query;
+    try {
+      const snippet = await db.query("SELECT * FROM snippet WHERE id = $1", [
+        id,
+      ]);
+      res.status(200);
+      res.json({ status: "success", data: snippet[0] });
+    } catch (error) {
+      res.status(400);
+      throw new Error(error);
+    }
+  })
+  .delete(async (req, res) => {
+    const { id } = req.query;
+    const user_id = req.user.id;
+    try {
+      const deletedSnippet = await db.query(
+        "DELETE FROM snippet WHERE id = $1 and user_id = $2",
+        [id, user_id]
+      );
+
+      if (deletedSnippet.length === 0) {
+        return res.status(403).json({
+          status: "error",
+          message: "Not authorized to access this route",
+        });
+      }
+
+      res.status(200);
+      res.json({
+        status: "success",
+        message: "Snippet successfully deleted",
+      });
+    } catch (error) {
+      res.status(400);
+      throw new Error(error);
+    }
+  })
+  .put(async (req, res) => {
+    const { id } = req.query;
+    const user_id = req.user.id;
+    try {
+      const { name, description, language, code } = req.body;
+      const updatedSnippet = await db.query(
+        "UPDATE snippet SET name = $1, description = $2, language = $3, code =$4 WHERE id = $5 AND user_id = $6 RETURNING *",
+        [name, description, language, code, id, user_id]
+      );
+
+      if (updatedSnippet.length === 0) {
+        return res.status(403).json({
+          status: "error",
+          message: "Not authorized to access this route",
+        });
+      }
+
+      res.status(201);
+      res.json({ status: "success", data: updatedSnippet[0] });
+    } catch (error) {
+      res.status(400);
+      throw new Error(error);
+    }
+  });
