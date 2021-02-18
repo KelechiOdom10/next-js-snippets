f55c144c7841a0db61b461125466958c3db19692
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-18 19:12:44 +0000

    Updated Database Script to iniatilize tables and get started with the project

--- lib/database.sql
+++ lib/database.sql
@@ -1,3 +1,4 @@
+-- create language enum for snippets
 CREATE TYPE language AS ENUM (
 	'HTML',
 	'CSS',
@@ -8,22 +9,45 @@ CREATE TYPE language AS ENUM (
 	'PHP',
 	'Perl',
 	'Dart',
-	'Go'
+	'Go',
+	'XML',
+	'Java',
+	'C#',
+	'C++',
+	'C',
+	'.NET',
+	'TypeScript',
+	'Ruby',
+	'Scala'
 );
+-- create timestamp updater function
+CREATE OR REPLACE FUNCTION trigger_set_timestamp() RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+--create user table
 CREATE TABLE users (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	username VARCHAR(25) NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
-	hashed_password VARCHAR(255) NOT NULL
+	hashed_password VARCHAR(255) NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
+--create snippet table
 CREATE TABLE snippet (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
 	description TEXT NOT NULL,
 	language language,
 	code TEXT NOT NULL,
-	user_id int FOREIGN KEY REFERENCES users(id)
+	user_id int NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	FOREIGN KEY (user_id) REFERENCES users(id)
 );
+--Create trigger to execute function we defined
+CREATE TRIGGER set_timestamp BEFORE
+UPDATE ON snippet FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
 -- Dummy Data INSERT QUERIES
 INSERT INTO users (id, username, email, hashed_password)
 VALUES (
