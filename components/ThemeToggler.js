efdeaef572258ebb32fb78f51efd7dc54caaff09
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-18 21:57:13 +0000

    Updated Theme toggler

--- components/ThemeToggler.js
+++ components/ThemeToggler.js
@@ -3,16 +3,16 @@ import { useColorMode, IconButton } from "@chakra-ui/react";
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 export default function ThemeToggler(props) {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <IconButton
-      colorScheme="teal"
-      aria-label="Toggle Color Mode"
-      icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-      borderRadius="full"
-      variant="ghost"
-      onClick={toggleColorMode}
-      {...props}
-    />
-  );
+	const { colorMode, toggleColorMode } = useColorMode();
+	return (
+		<IconButton
+			colorScheme="teal"
+			aria-label="Toggle Color Mode"
+			icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+			borderRadius="full"
+			variant="ghost"
+			onClick={toggleColorMode}
+			{...props}
+		/>
+	);
 }
