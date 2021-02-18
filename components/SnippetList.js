af22de5ed300ef228bd0aa34934f48ee53ad03e1
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-18 19:48:52 +0000

    Updated container component to house snippets and added the ability to search through text and filter via drop-down both standalone and simultaneously

--- components/SnippetList.js
+++ components/SnippetList.js
@@ -1,11 +1,82 @@
-import React from "react";
+import { Box, Button, Flex, Input, Select } from "@chakra-ui/react";
+import React, { useState } from "react";
+
 import Snippet from "./Snippet";
 
-export default function SnippetList({ snippets }) {
+export default function SnippetList({ snippets, disabled, ...props }) {
+	let filteredSnippets = snippets;
+	const [searchValue, setSearchValue] = useState("");
+	const [language, setLanguage] = useState("");
+
+	if (searchValue.length > 0) {
+		filteredSnippets = filteredSnippets.filter(
+			snippet =>
+				snippet.name.toLowerCase().includes(searchValue) ||
+				snippet.description.toLowerCase().includes(searchValue) ||
+				snippet.language.toLowerCase().includes(searchValue)
+		);
+	}
+
+	const resetFilters = () => {
+		setLanguage("");
+		setSearchValue("");
+		setUserSnippets(["empty"]);
+		filteredSnippets = snippets;
+	};
+
 	return (
-		<>
+		<Box mb={8} {...props}>
+			<Flex flexDir="column" align="center" my={6}>
+				<Input
+					type="text"
+					value={searchValue}
+					onChange={e => setSearchValue(e.target.value.toLowerCase())}
+					placeholder="Search snippets by name, description or language"
+					_placeholder={{ fontStyle: "italic" }}
+					fontSize={{ base: "xs", md: "sm", lg: "md" }}
+				/>
+			</Flex>
+
+			<Flex my={6} align="center">
+				<Select
+					placeholder="Filter by Language"
+					variant="filled"
+					value={language}
+					width="45%"
+					fontSize={{ base: "xs", md: "sm", lg: "md", xl: "lg" }}
+					fontWeight="semibold"
+					onChange={e => setLanguage(e.currentTarget.value)}
+					isRequired
+				>
+					<option value="JavaScript">JavaScript</option>
+					<option value="HTML">HTML</option>
+					<option value="CSS">CSS</option>
+					<option value="Python">Python</option>
+					<option value="SQL">SQL</option>
+					<option value="XML">XML</option>
+					<option value="JSX">JSX</option>
+					<option value="PHP">PHP</option>
+				</Select>
+				<Button
+					colorScheme="teal"
+					variant="outline"
+					ml={5}
+					rounded="md"
+					borderWidth={2}
+					fontSize={{ base: "xs", md: "sm", lg: "md", xl: "lg" }}
+					fontWeight="semibold"
+					onClick={resetFilters}
+				>
+					Reset filters
+				</Button>
+			</Flex>
+
 			{snippets &&
-				snippets.map(snippet => <Snippet key={snippet.id} snippet={snippet} />)}
-		</>
+				filteredSnippets
+					.filter(snippet => (language ? snippet.language === language : true))
+					.map(snippet => (
+						<Snippet key={snippet.id} snippet={snippet} disabled={disabled} />
+					))}
+		</Box>
 	);
 }
