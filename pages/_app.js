b28aede72420f46c1bedd01a4fd1781b6527abf8
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-18 19:53:24 +0000

    Added the react-query QueryClientProvider to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,37 @@
 import "../styles/globals.css";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import React from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { UserContextProvider } from "../context/userState";
+
+const theme = extendTheme({
+	components: {
+		Button: {
+			baseStyle: { _focus: { boxShadow: "none", outline: "0 !important" } },
+		},
+		Tabs: {
+			baseStyle: { _focus: { boxShadow: "none", outline: "0 !important" } },
+		},
+	},
+});
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	const queryClientRef = React.useRef();
+	if (!queryClientRef.current) {
+		queryClientRef.current = new QueryClient();
+	}
+	return (
+		<ChakraProvider theme={theme}>
+			<QueryClientProvider client={queryClientRef.current}>
+				<Hydrate state={pageProps.dehydratedState}>
+					<UserContextProvider>
+						<Component {...pageProps} />
+					</UserContextProvider>
+				</Hydrate>
+			</QueryClientProvider>
+		</ChakraProvider>
+	);
 }
 
 export default MyApp;
