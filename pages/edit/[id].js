c900b91b6e6dcff632cb0945cb33e375971d3c21
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-18 22:02:15 +0000

    Created the edit snippet page

---
+++ pages/edit/[id].js
@@ -0,0 +1,79 @@
+import { Alert, AlertDescription, Flex } from "@chakra-ui/react";
+import UserNavBar from "../../components/UserNavBar";
+import cookie from "cookie";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { fetchSnippetById } from "../../services/api";
+import { useQuery, useQueryClient } from "react-query";
+import { SpinnerIcon } from "@chakra-ui/icons";
+import EditForm from "../../components/EditForm";
+
+export const getServerSideProps = async ({ req, params }) => {
+	const id = params.id;
+	const parseCookies = req => {
+		return cookie.parse(req ? req.headers.cookie || "" : document.cookie);
+	};
+
+	const isLoggedIn = parseCookies(req);
+
+	if (!isLoggedIn.auth) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
+
+	const snippet = await fetchSnippetById(id);
+
+	return {
+		props: {
+			snippet,
+			id,
+		},
+	};
+};
+
+export default function UpdateSnippet({ snippet, id }) {
+	if (!id) {
+		return <></>;
+	}
+
+	const queryClient = useQueryClient();
+
+	const { error, data, isLoading } = useQuery(
+		["snippet", id],
+		() => fetchSnippetById(id),
+		{
+			initialData: () => {
+				if (queryClient.getQueryData(["snippet", id], { exact: true })) {
+					return queryClient.getQueryData(["snippet", id], { exact: true });
+				} else {
+					return snippet;
+				}
+			},
+			refetchOnWindowFocus: false,
+		}
+	);
+
+	queryClient.getQueryData("token");
+	queryClient.getQueryData("user");
+
+	return (
+		<Flex flexDir="column" h="100vh" m="0 auto">
+			<UserNavBar />
+			{error && (
+				<Alert status="error">
+					<AlertDescription>{error.message}, Please refresh</AlertDescription>
+				</Alert>
+			)}
+			{isLoading && (
+				<Flex align="center" justify="center">
+					<SpinnerIcon color="teal" />
+				</Flex>
+			)}
+			{data && <EditForm snippet={data} />}
+			<ReactQueryDevtools />
+		</Flex>
+	);
+}
