8f31a8f88878ad21b7b8f21a34e565b7ce83c599
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-18 20:15:34 +0000

    Updated the code editor for creating code snippets with a helper function to change the mode depending on the language

--- components/CodeEditor.js
+++ components/CodeEditor.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
 import { Controlled as ControlledEditor } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/dracula.css";
+import "codemirror/theme/material-palenight.css";
 import "codemirror/mode/htmlmixed/htmlmixed";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import "codemirror/mode/python/python";
+import "codemirror/mode/sql/sql";
+import "codemirror/mode/clike/clike";
 
-function CodeEditor({ theme, language }) {
-	const [value, setValue] = useState("");
-
+function CodeEditor({ theme, language, code, setCode }) {
 	const handleChange = (editor, data, value) => {
-		setValue(value);
+		setCode(value);
+	};
+
+	const switchLanguage = language => {
+		switch (language?.toLowerCase()) {
+			case "html":
+			case "xml":
+				return "htmlmixed";
+			case "jsx":
+				return "javascript";
+			default:
+				return language?.toLowerCase();
+		}
 	};
 
 	return (
 		<>
 			<ControlledEditor
 				onBeforeChange={handleChange}
-				value={value}
+				value={code}
 				className="code-mirror-wrapper"
 				options={{
 					lineWrapping: true,
 					lint: true,
-					mode: language === "html" ? "htmlmixed" : language,
+					mode: `${switchLanguage(language)}`,
 					theme: theme,
 					lineNumbers: true,
 					tabSize: 4,
