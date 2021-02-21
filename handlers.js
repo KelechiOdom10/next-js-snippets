0344e0720d406ae6abf9cc70395877c324e8de54
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-21 18:59:56 +0000

    Switch handler to a function to allow /api/something and /api/something/:id to exist together

--- handlers.js
+++ handlers.js
@@ -1,12 +1,15 @@
+import bodyParser from "body-parser";
 import nextConnect from "next-connect";
 
-export const handler = nextConnect({
-  onNoMatch(req, res) {
-    res
-      .status(405)
-      .json({ status: "error", message: `Method ${req.method} Not Allowed` });
-  },
-  onError(error, req, res) {
-    res.status(501).json({ status: "error", message: error.message });
-  },
-});
+export default function getHandler() {
+	return nextConnect({
+		onNoMatch(req, res) {
+			res
+				.status(405)
+				.json({ status: "error", message: `Method ${req.method} Not Allowed` });
+		},
+		onError(error, req, res) {
+			res.status(501).json({ status: "error", message: error.message });
+		},
+	}).use(bodyParser.urlencoded({ extended: false }));
+}
