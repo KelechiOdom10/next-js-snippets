faa9495bf3d4f823ea472d8cc95f0ef17bb654bc
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-26 21:22:39 +0000

    Handler test

--- handlers.js
+++ handlers.js
@@ -13,6 +13,6 @@ export default function getHandler() {
 			res.status(501).json({ status: "error", message: error.message });
 		},
 	})
-		.use(bodyParser.urlencoded({ extended: false }))
-		.use(cors({ origin: [process.env.NEXT_PUBLIC_URL, process.env.NEXT_URL] }));
+		.use(cors())
+		.use(bodyParser.urlencoded({ extended: false }));
 }
