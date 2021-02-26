78840097ca70c02cb244f6562b4020993368f0d8
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-26 19:45:18 +0000

    Updated pages to accomodate footer

--- pages/signup.js
+++ pages/signup.js
@@ -1,54 +1,36 @@
-import {
-	Box,
-	Heading,
-	useColorMode,
-	Flex,
-	Link,
-	Button,
-	ButtonGroup,
-	IconButton,
-} from "@chakra-ui/react";
+import { Box, Heading, Flex, Image } from "@chakra-ui/react";
 import SignupForm from "../components/SignupForm";
-import { ArrowBackIcon } from "@chakra-ui/icons";
 import ThemeToggler from "../components/ThemeToggler";
 import cookie from "cookie";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
+import Footer from "../components/Footer";
 
 export default function LoginArea() {
-	const { colorMode } = useColorMode();
 	const router = useRouter();
 
 	return (
-		<Layout title="Snippets | Sign up" description="Snippets Sign up Page">
+		<Layout
+			title="Snippets | Sign up"
+			description="Snippets Sign up Page"
+			minHeight="100vh"
+		>
 			<Flex
 				flexDir="column"
-				h="100vh"
-				maxWidth={{ base: "90%", md: "70%" }}
+				maxWidth={{ base: "90%", md: "80%" }}
 				m="0 auto"
+				pb="100px"
 			>
-				<Flex my={8} align="center" justify="space-between">
-					<ButtonGroup
-						size="sm"
-						isAttached
-						variant="outline"
-						colorScheme={colorMode === "dark" ? "teal" : "black"}
-						onClick={() => router.back()}
-					>
-						<IconButton
-							fontWeight="bold"
-							aria-label="Add to friends"
-							fontSize={{ base: "xs", md: "sm" }}
-							icon={<ArrowBackIcon />}
+				<Flex mb={4} align="center" justify="space-between">
+					<Flex align="center" h="60px">
+						<Image
+							src="/coding-logo.png"
+							h="60%"
+							alt="Snippets Logo"
+							cursor="pointer"
+							onClick={() => router.push("/")}
 						/>
-						<Button
-							mr="-px"
-							fontWeight="bold"
-							fontSize={{ base: "xs", md: "sm" }}
-						>
-							Back
-						</Button>
-					</ButtonGroup>
+					</Flex>
 					<ThemeToggler />
 				</Flex>
 				<Box
@@ -66,6 +48,7 @@ export default function LoginArea() {
 					<SignupForm />
 				</Box>
 			</Flex>
+			<Footer />
 		</Layout>
 	);
 }
