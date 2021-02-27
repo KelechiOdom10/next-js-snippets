9af392b471d1b76edc8dccb3a3d1467a555c3fc6
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-27 04:42:39 +0000

    Cors fix

--- pages/api/login.js
+++ pages/api/login.js
@@ -43,7 +43,7 @@ export default getHandler().post(async (req, res) => {
 		res.setHeader(
 			"Set-Cookie",
 			cookie.serialize("auth", token, {
-				domain: process.env.NEXT_URL
+				domain: process.env.NEXT_URL,
 				httpOnly: true,
 				secure: process.env.NODE_ENV !== "development",
 				sameSite: process.env.NODE_ENV !== "development" ? "none" : true,
