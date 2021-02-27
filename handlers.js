0bccf585c12521e1058e0d5f5a221a7c8ee35076
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-27 12:37:00 +0000

    Cors fix

--- handlers.js
+++ handlers.js
@@ -3,18 +3,20 @@ import cors from "cors";
 import nextConnect from "next-connect";
 
 export default function getHandler() {
-	return nextConnect({
-		onNoMatch(req, res) {
-			res.status(405).json({
-				status: "error",
-				message: `Method ${req.method} Not Allowed`,
-			});
-		},
-		onError(error, req, res) {
-			res.status(501).json({ status: "error", message: error.message });
-		},
-	})
-		.use(cors({ origin: [/.*next-js-snippets.*/], credentials: true }))
-		.options("*", cors())
-		.use(bodyParser.urlencoded({ extended: false }));
+	return (
+		nextConnect({
+			onNoMatch(req, res) {
+				res.status(405).json({
+					status: "error",
+					message: `Method ${req.method} Not Allowed`,
+				});
+			},
+			onError(error, req, res) {
+				res.status(501).json({ status: "error", message: error.message });
+			},
+		})
+			.use(cors({ origin: [/.*next-js-snippets.*/], credentials: true }))
+			// .options("*", cors())
+			.use(bodyParser.urlencoded({ extended: false }))
+	);
 }
