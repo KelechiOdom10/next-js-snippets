8e4ca27cc5d79e12f9695d0fdb082a8d457009d8
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-27 04:09:22 +0000

    Cors fix

--- handlers.js
+++ handlers.js
@@ -3,36 +3,17 @@ import cors from "cors";
 import nextConnect from "next-connect";
 
 export default function getHandler() {
-	return (
-		nextConnect({
-			onNoMatch(req, res) {
-				res
-					.status(405)
-					.json({
-						status: "error",
-						message: `Method ${req.method} Not Allowed`,
-					});
-			},
-			onError(error, req, res) {
-				res.status(501).json({ status: "error", message: error.message });
-			},
-		})
-			// .use(cors({ origin: [/.*next-js-snippets.*/], credentials: true, maxAge: 86400 }))
-			.use((req, res, next) => {
-				res.header("Access-Control-Allow-Origin", process.env.NEXT_URL);
-				res.header(
-					"Access-Control-Allow-Headers",
-					"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-				);
-				if (req.method === "OPTIONS") {
-					res.header(
-						"Access-Control-Allow-Methods",
-						"PUT, POST, PATCH, DELETE, GET"
-					);
-					return res.status(200).json({});
-				}
-				next();
-			})
-			.use(bodyParser.urlencoded({ extended: false }))
-	);
+	return nextConnect({
+		onNoMatch(req, res) {
+			res
+				.status(405)
+				.json({ status: "error", message: `Method ${req.method} Not Allowed` });
+		},
+		onError(error, req, res) {
+			res.status(501).json({ status: "error", message: error.message });
+		},
+	})
+		.use(cors({ origin: [/.*next-js-snippets.*/], credentials: true }))
+		.options("*", cors({ origin: [/.*next-js-snippets.*/], maxAge: 86400 }))
+		.use(bodyParser.urlencoded({ extended: false }));
 }
