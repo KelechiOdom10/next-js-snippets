2ac27c1b3b958a9c8395741e0cfe932518e46e11
kelechiodom10 <kelechiodom10@gmail.com>
2021-02-27 03:40:30 +0000

    Cors fix

--- handlers.js
+++ handlers.js
@@ -6,19 +6,17 @@ export default function getHandler() {
 	return (
 		nextConnect({
 			onNoMatch(req, res) {
-				res
-					.status(405)
-					.json({
-						status: "error",
-						message: `Method ${req.method} Not Allowed`,
-					});
+				res.status(405).json({
+					status: "error",
+					message: `Method ${req.method} Not Allowed`,
+				});
 			},
 			onError(error, req, res) {
 				res.status(501).json({ status: "error", message: error.message });
 			},
 		})
 			.use(function (req, res, next) {
-				res.header("Access-Control-Allow-Origin", process.env.NEXT_URL);
+				res.header("Access-Control-Allow-Origin", [/.*next-js-snippets.*/]);
 				res.header(
 					"Access-Control-Allow-Headers",
 					"Origin, X-Requested-With, Content-Type, Accept"
