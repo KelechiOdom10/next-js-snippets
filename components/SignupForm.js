1dee23538e4e0a93c63af139ce37dcb39e49ce5a
kelechiodom10 <kelechiodom10@gmail.com>
2021-03-13 20:58:25 +0000

    Updated submit button to fit full mobile view widtho

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -139,7 +139,7 @@ export default function SignupForm() {
 				<Button
 					fontWeight="bold"
 					colorScheme="teal"
-					width={{ base: formState.isSubmitting ? "50%" : "40%", md: "full" }}
+					width={{ base: "full", md: formState.isSubmitting ? "40%" : "30%" }}
 					mt={4}
 					size={{ base: "sm", md: "md" }}
 					px={4}
