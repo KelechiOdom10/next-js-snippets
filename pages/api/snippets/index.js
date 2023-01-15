63f7b4d330c766f12a9e9679219558a5a72a01e7
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-15 13:01:49 +0000

    Add cache control to index page and fix error

--- pages/api/snippets/index.js
+++ pages/api/snippets/index.js
@@ -6,29 +6,31 @@ import { authorize } from "../../../middleware/authorize";
 const authorization = nc().post("/api/snippets", authorize);
 
 export default getHandler()
-	.use(authorization)
-	.get(async (req, res) => {
-		try {
-			const snippets = await db.query("SELECT * FROM snippet");
-			res.status(200);
-			res.json({ status: "success", data: snippets });
-		} catch (error) {
-			res.status(400);
-			throw new Error(error);
-		}
-	})
-	.post(async (req, res) => {
-		const user_id = req.user.id;
-		try {
-			const { name, description, language, code } = req.body;
-			const snippet = await db.query(
-				"INSERT INTO snippet (name, description, language, code, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-				[name, description, language, code, user_id]
-			);
-			res.status(201);
-			res.json({ status: "success", data: snippet });
-		} catch (error) {
-			res.status(400);
-			throw new Error(error);
-		}
-	});
+  .use(authorization)
+  .get(async (req, res) => {
+    console.log("herreee");
+    try {
+      const snippets = await db.query("SELECT * FROM snippet");
+      console.log({ snippets });
+      res.status(200);
+      res.json({ status: "success", data: snippets });
+    } catch (error) {
+      res.status(400);
+      throw new Error(error);
+    }
+  })
+  .post(async (req, res) => {
+    const user_id = req.user.id;
+    try {
+      const { name, description, language, code } = req.body;
+      const snippet = await db.query(
+        "INSERT INTO snippet (name, description, language, code, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [name, description, language, code, user_id]
+      );
+      res.status(201);
+      res.json({ status: "success", data: snippet });
+    } catch (error) {
+      res.status(400);
+      throw new Error(error);
+    }
+  });
--- pages/index.js
+++ pages/index.js
@@ -8,66 +8,71 @@ import cookie from "cookie";
 import Layout from "../components/Layout";
 import Footer from "../components/Footer";
 
-export const getServerSideProps = async ({ req }) => {
-	const parseCookies = req => {
-		return cookie.parse(req ? req.headers.cookie || "" : document.cookie);
-	};
+export const getServerSideProps = async ({ req, res }) => {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=300, stale-while-revalidate=59"
+  );
 
-	const isLoggedIn = parseCookies(req);
+  const parseCookies = req => {
+    return cookie.parse(req ? req.headers.cookie || "" : document.cookie);
+  };
 
-	if (isLoggedIn["auth"]) {
-		return {
-			redirect: {
-				destination: "/home",
-				permanent: false,
-			},
-		};
-	}
-	
-	let snippets = []
+  const isLoggedIn = parseCookies(req);
 
-	try {
-		snippets = await fetchAllSnippets();
-	} catch(e) {
-		console.log({ e })
-	}
-	return {
-		props: { snippets },
-	};
+  if (isLoggedIn["auth"]) {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+
+  let snippets = [];
+
+  try {
+    snippets = await fetchAllSnippets();
+  } catch (e) {
+    console.log({ e });
+  }
+  return {
+    props: { snippets },
+  };
 };
 
 export default function Home({ snippets }) {
-	const { error, data } = useQuery("snippets", fetchAllSnippets, {
-		initialData: () => {
-			return snippets;
-		},
-	});
+  const { error, data } = useQuery("snippets", fetchAllSnippets, {
+    initialData: () => {
+      return snippets;
+    },
+  });
 
-	return (
-		<Layout
-			title="Snippets | Code Snippets for Developers"
-			description="Copy Code Snippets to enhance your workflow"
-			minHeight="100vh"
-		>
-			<Box pb="100px">
-				<Navbar />
-				<Hero />
-				{error && (
-					<Alert status="error">
-						<AlertDescription>{error.message}</AlertDescription>
-					</Alert>
-				)}
-				{data && (
-					<SnippetList
-						snippets={data}
-						disabled={true}
-						w={{ base: "90%", md: "65%", lg: "60%" }}
-						mx="auto"
-						mb={10}
-					/>
-				)}
-			</Box>
-			<Footer showList />
-		</Layout>
-	);
+  return (
+    <Layout
+      title="Snippets | Code Snippets for Developers"
+      description="Copy Code Snippets to enhance your workflow"
+      minHeight="100vh"
+    >
+      <Box pb="100px">
+        <Navbar />
+        <Hero />
+        {error && (
+          <Alert status="error">
+            <AlertDescription>{error.message}</AlertDescription>
+          </Alert>
+        )}
+        {data && (
+          <SnippetList
+            snippets={data}
+            disabled={true}
+            w={{ base: "90%", md: "65%", lg: "60%" }}
+            mx="auto"
+            mb={10}
+          />
+        )}
+      </Box>
+      <Footer showList />
+    </Layout>
+  );
 }
